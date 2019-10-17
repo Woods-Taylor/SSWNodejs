162862262300cf93d26d6589d3bc65dc6abccd14
Woods-Taylor <woods_taylor@columbusstate.edu>
2019-10-17 22:14:01 +0000

    t

--- app.js
+++ app.js
@@ -1,27 +1,29 @@
 var http = require('http');
 var path = require('path');
 const express = require("express");
-var bodyPArser = require("body-parser")
+var bodyParser = require("body-parser");
 const app = express();
 const port = process.env.PORT || 3000;
 
-
-
 app.set('views', path.join(__dirname, 'views'));
-app.set("view engine", 'ejs');
-app.use(bodyParser.urlencoded({encoded: true}));
-
+app.set("view engine", "ejs");
+app.use(bodyParser.urlencoded({ encoded: true}));
 var task = ["clean", "cook"];
+var complete = ["eat", "sleep"];
+
 
 app.get('/', function(req, res){
-    res.render("index")
-
+    res.render('index',{task:task, complete: complete});
 });
 
 app.post('/addtask', function(req, res){
     var newTask = req.body.newtask;
     task.push(newTask);
-    res.redirect('/');
+    res.redirect("/");
+});
+
+app.post('/removetask', function(req, res){
+    res.redirect("/");
 });
 
 http.createServer(app).listen(port, function(){
