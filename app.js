1b075e679f1c07ba19a01b9b94c392195be3ab6b
Woods-Taylor <woods_taylor@columbusstate.edu>
2019-10-17 22:56:15 +0000

    t

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const port = process.env.PORT || 3000;
 
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine", "ejs");
+app.use(express.static("public"));
 app.use(bodyParser.urlencoded({ encoded: true}));
 var task = ["clean", "cook"];
 var complete = ["eat", "sleep"];
