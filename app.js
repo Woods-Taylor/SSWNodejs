b7b7db2d76f8af00039d0524f8cc8e8562b0c0fb
Woods-Taylor <woods_taylor@columbusstate.edu>
2019-10-17 22:48:27 +0000

    t

--- app.js
+++ app.js
@@ -13,7 +13,7 @@ var complete = ["eat", "sleep"];
 
 
 app.get('/', function(req, res){
-    res.render('index',{task:task, complete: complete});
+    res.render('index',{task:task, complete:complete});
 });
 
 app.post('/addtask', function(req, res){
@@ -29,8 +29,8 @@ app.post('/removetask', function(req, res){
         task.splice(task.indexOf(completeTask), 1);
     }else if(typeof completeTask === "object"){
         for(var i = 0; i < completeTask.length ; i++){
-            completeTask.push(completeTask[i]);
-            task.splice(task.indexOf(completeTask[i]), 1)
+            complete.push(completeTask[i]);
+            task.splice(task.indexOf(completeTask[i]), 1);
         }
     }
     res.redirect("/");
